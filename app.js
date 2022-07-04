013c957708baf6a442e1455691de25f38184e42f
Yuri Simonov <y.simonov@alef.im>
2022-07-04 08:33:58 +0300

    функции

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 "use strict";
-const a = 1;
+const str1 = "1";
+const str2 = "2";
+function funcTs(a, b) {
+    return a + b;
+}
+const funcTsArrow = (a, b) => {
+    return a + b;
+};
+console.log(funcTs(str1, str2));
+console.log(funcTsArrow(str1, str2));
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-const a = 1;
+const str1 = "1";
+const str2 = "2";
+
+function funcTs(a: string, b: string): string {
+	return a + b;
+}
+
+const funcTsArrow = (a: string, b: string): string => {
+	return a + b;
+};
+
+console.log(funcTs(str1, str2));
+console.log(funcTsArrow(str1, str2));
