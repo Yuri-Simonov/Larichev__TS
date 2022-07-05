655559e3d1073674b2d3edf41c263a6691ae0123
Yuri Simonov <y.simonov@alef.im>
2022-07-05 08:48:37 +0300

    выполнено задание на типизацию объекта

---
+++ test.ts
@@ -0,0 +1,19 @@
+let info: {
+	officeId: number;
+	isOpened: boolean;
+	contacts: {
+		phone: string;
+		email: string;
+		address: { city: string };
+	};
+} = {
+	officeId: 1,
+	isOpened: true,
+	contacts: {
+		phone: "string",
+		email: "string",
+		address: {
+			city: "string",
+		},
+	},
+};
