403a1c3862d61ea8971a0693b916a30a187a936c
Yuri Simonov <y.simonov@alef.im>
2022-07-07 08:38:15 +0300

    изучен модификатор readonly

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
-const someTuple: [number, string, ...boolean[]] = [1, "2", true];
+const someTuple: readonly [number, string] = [1, "2"];
+
+// someTuple.push(2);
 
 const [num, str] = someTuple;
 console.log(num, str);
