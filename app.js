65caa75474efbdaa17a5647aa079e3c8b4d332d6
Yuri Simonov <y.simonov@alef.im>
2022-07-09 18:16:59 +0300

    union

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 "use strict";
-const someTuple = [1, "2"];
-const [num, str] = someTuple;
-console.log(num, str);
+function narr(id) {
+    if (typeof id === "string") {
+        console.log(id, " = string");
+    }
+    else {
+        console.log(id, " = number | boolean");
+    }
+}
+function logObject(obj) {
+    if ("a" in obj) {
+        console.log(obj.a);
+    }
+    else {
+        console.log(obj.b);
+    }
+}
+function multiple(a, b) {
+    if (typeof a === typeof b) {
+        console.log(typeof a);
+    }
+    else {
+        console.log("not right");
+    }
+}
+multiple(1, 2);
--- app.ts
+++ app.ts
@@ -1,10 +1,24 @@
-enum StatusCode {
-	success,
-	in_process,
-	failed,
+function narr(id: string | number | boolean) {
+	if (typeof id === "string") {
+		console.log(id, " = string");
+	} else {
+		console.log(id, " = number | boolean");
+	}
 }
 
-const res = {
-	message: "Платеж прошел успешно!",
-	status: StatusCode.success,
-};
+function logObject(obj: { a: number } | { b: number }) {
+	if ("a" in obj) {
+		console.log(obj.a);
+	} else {
+		console.log(obj.b);
+	}
+}
+
+function multiple(a: string | number, b: number | boolean) {
+	if (typeof a === typeof b) {
+		console.log(typeof a);
+	} else {
+		console.log("not right");
+	}
+}
+multiple(1, 2);
