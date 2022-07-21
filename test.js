06284c196f90bd01f13b687bf3a014ffc6a18b3a
Yuri Simonov <yura15-96@mail.ru>
2022-07-21 12:10:55 +0300

    Упражнение - Делаем корзину товаров

--- test.js
+++ test.js
@@ -1,20 +1,63 @@
 "use strict";
-class User {
-    constructor() {
-        this.skills = [];
-    }
-    addSkill(skillOrSkils) {
-        if (typeof skillOrSkils === "string") {
-            this.skills = this.skills.concat(skillOrSkils);
-        }
-        if (Array.isArray(skillOrSkils)) {
-            this.skills = this.skills.concat(skillOrSkils);
-        }
+class Product {
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 }
-let user1 = new User();
-user1.addSkill("one skill");
-console.log("user1", user1);
-let user2 = new User();
-user2.addSkill(["one skill", "two skill"]);
-console.log("user2", user2);
+class Delivery {
+    constructor(date) {
+        this.date = date;
+    }
+}
+class HomeDelivery extends Delivery {
+    constructor(date, address) {
+        super(date);
+        this.address = address;
+    }
+}
+class ShopDelivery extends Delivery {
+    constructor(shopId) {
+        super(new Date());
+        this.shopId = shopId;
+    }
+}
+class Cart {
+    constructor() {
+        this.products = [];
+    }
+    addToCart(product) {
+        this.products.push(product);
+    }
+    deleteProductFromCart(productId) {
+        this.products = this.products.filter((p) => {
+            return p.id !== productId;
+        });
+    }
+    countToTotalPrice() {
+        return this.products
+            .map((p) => p.price)
+            .reduce((p1, p2) => p1 + p2);
+    }
+    setDelivery(delivery) {
+        this.delivery = delivery;
+    }
+    checkOut() {
+        if (this.products.length === 0) {
+            throw new Error("Нет ни одного товара в корзине");
+        }
+        if (!this.delivery) {
+            throw new Error("Не указан способ доставки");
+        }
+        return { success: true };
+    }
+}
+const cart = new Cart();
+cart.addToCart(new Product(1, "печеньки", 60));
+cart.addToCart(new Product(2, "хлеб", 80));
+cart.addToCart(new Product(3, "молоко", 100));
+cart.deleteProductFromCart(2);
+cart.setDelivery(new HomeDelivery(new Date(), "some adress"));
+console.log(cart.countToTotalPrice());
+console.log(cart.checkOut());
--- test.ts
+++ test.ts
@@ -1,22 +1,61 @@
-class User {
-	skills: string[] = [];
+class Product {
+	constructor(public id: number, public name: string, public price: number) {}
+}
+class Delivery {
+	constructor(public date: Date) {}
+}
 
-	addSkill(skill: string): void;
-	addSkill(skills: string[]): void;
-	addSkill(skillOrSkils: string | string[]): void {
-		if (typeof skillOrSkils === "string") {
-			this.skills = this.skills.concat(skillOrSkils);
-		}
-		if (Array.isArray(skillOrSkils)) {
-			this.skills = this.skills.concat(skillOrSkils);
-		}
+class HomeDelivery extends Delivery {
+	constructor(date: Date, public address: string) {
+		super(date);
+	}
+}
+class ShopDelivery extends Delivery {
+	constructor(public shopId: number) {
+		super(new Date());
 	}
 }
 
-let user1 = new User();
-user1.addSkill("one skill");
-console.log("user1", user1);
+type DeliveryOptions = HomeDelivery | ShopDelivery;
 
-let user2 = new User();
-user2.addSkill(["one skill", "two skill"]);
-console.log("user2", user2);
+class Cart {
+	private products: Array<Product> = [];
+	private delivery: DeliveryOptions;
+
+	public addToCart(product: Product): void {
+		this.products.push(product);
+	}
+	public deleteProductFromCart(productId: number): void {
+		this.products = this.products.filter((p: Product) => {
+			return p.id !== productId;
+		});
+	}
+	public countToTotalPrice(): number {
+		return this.products
+			.map((p: Product) => p.price)
+			.reduce((p1: number, p2: number) => p1 + p2);
+	}
+
+	public setDelivery(delivery: DeliveryOptions): void {
+		this.delivery = delivery;
+	}
+
+	public checkOut() {
+		if (this.products.length === 0) {
+			throw new Error("Нет ни одного товара в корзине");
+		}
+		if (!this.delivery) {
+			throw new Error("Не указан способ доставки");
+		}
+		return { success: true };
+	}
+}
+
+const cart = new Cart();
+cart.addToCart(new Product(1, "печеньки", 60));
+cart.addToCart(new Product(2, "хлеб", 80));
+cart.addToCart(new Product(3, "молоко", 100));
+cart.deleteProductFromCart(2);
+cart.setDelivery(new HomeDelivery(new Date(), "some adress"));
+console.log(cart.countToTotalPrice());
+console.log(cart.checkOut());
