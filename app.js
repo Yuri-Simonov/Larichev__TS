7d90dcf9166246f3a74f416552bd8387f4db2c92
Yuri Simonov <yura15-96@mail.ru>
2022-07-21 15:20:18 +0300

    static

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
 "use strict";
-class Payment {
-    constructor(id) {
-        this.status = "new";
-        this.id = id;
-    }
-    pay() {
-        this.status = "paid";
+class User {
+    changeName() {
+        User.userName = "Not Yuri";
     }
 }
-class PersistedPayment extends Payment {
-    constructor() {
-        const id = Math.random();
-        super(id);
-    }
-    save() {
-        // Что-то сохраняет
-    }
-    pay(date) {
-        // super.pay(); вариант
-        this.status = "new paid";
-        if (date) {
-            this.paidAt = new Date();
-        }
-    }
-}
+User.userName = "Yuri";
+console.log(User.userName); // Yuri
+console.log(new User().changeName()); // undefined
--- app.ts
+++ app.ts
@@ -1,36 +1,10 @@
-type PaymentStatus = "new" | "paid" | "new paid";
+class User {
+	static userName = "Yuri";
 
-class Payment {
-	id: number;
-	status: PaymentStatus = "new";
-
-	constructor(id: number) {
-		this.id = id;
-	}
-
-	pay() {
-		this.status = "paid";
+	changeName() {
+		User.userName = "Not Yuri";
 	}
 }
 
-class PersistedPayment extends Payment {
-	dataBaseId: number;
-	paidAt: Date;
-
-	constructor() {
-		const id = Math.random();
-		super(id);
-	}
-
-	save() {
-		// Что-то сохраняет
-	}
-
-	override pay(date?: Date): void {
-		// super.pay(); вариант
-		this.status = "new paid";
-		if (date) {
-			this.paidAt = new Date();
-		}
-	}
-}
+console.log(User.userName); // Yuri
+console.log(new User().changeName()); // undefined
